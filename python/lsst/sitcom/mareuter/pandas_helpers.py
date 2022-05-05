a8028d71482e7c53b511ed89832fda103d169674
Michael Reuter <mareuter@lsst.org>
2022-05-04 17:08:23 -0700

    Add ackcmd translation helper function.

---
+++ python/lsst/sitcom/mareuter/pandas_helpers.py
@@ -0,0 +1,14 @@
+import json
+import pathlib
+
+__all__ = ["get_csc_ackcmd_mapping"]
+
+
+def get_csc_ackcmd_mapping(
+    csc: str,
+    xml_sal_version: str,
+    save_dir: pathlib.Path = pathlib.Path("~/DATA").expanduser(),
+):
+    input_file = save_dir / f"ackcmd_translation_{xml_sal_version}.json"
+    translations = json.load(input_file.open())
+    return {int(k): v for k, v in translations[csc].items()}
